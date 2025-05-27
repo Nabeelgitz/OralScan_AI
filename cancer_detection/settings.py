35ed6b3c80f2e5621dbdd149d93eaeca892d523b
Nabeelgitz <ancodena20@gmail.com>
2025-05-27 14:20:30 +0530

    Added STATIC_ROOT for Render deployment

--- cancer_detection/settings.py
+++ cancer_detection/settings.py
@@ -107,6 +107,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
